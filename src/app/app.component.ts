c6a1f6021cf0b3b72fee66bd1507887e8d63fde1
Slamtrade <42183416+slamtrade@users.noreply.github.com>
2019-02-21 10:23:39 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public data: any = {
+    email: "",
+    subject: "",
+    description: ""
+  }
+
+  constructor(db: AngularFireDatabase) {
+    this.data = db.list('/data');
+  }
+
+  save(): void {
+    let error = false;
+    if (this.data.subject.length > 80) {
+      alert("It is more than 80 char");
+      return;
+    }
+    if (this.data.description.length > 400) {
+      error = true;
+    }
+
+    if (error === false) {
+      this.data.push({
+        "email": this.data.email,
+        "subject": this.data.subject,
+        "description": this.data.description
+      });
+      console.log(this.data);
+    }
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,31 @@
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAIOPKbHp_SjpT1I8OaRrOxQ0HC3Ct2zSI",
+    authDomain: "cool-attic-179404.firebaseapp.com",
+    databaseURL: "https://cool-attic-179404.firebaseio.com",
+    projectId: "cool-attic-179404",
+    storageBucket: "cool-attic-179404.appspot.com",
+    messagingSenderId: "107139906786"
+  }
+};
+
+
+
+
+
+
+
+
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
